93f58b02adbf936c0f05d8a240edecd264bde88c
Sagarj21 <sagar21202@gmail.coml>
2023-10-04 12:44:06 +0530

    counting sort time complexity updated

--- src/SortingAlgos/BasicSorting.java
+++ src/SortingAlgos/BasicSorting.java
@@ -58,7 +58,7 @@ public class BasicSorting {
 		}
 	}
 	
-	//counting sort
+	//counting sort time complexity O(n+k) where k is range of elements
 	public static void  countingSort(int arr[]) {
 		int largest=Integer.MIN_VALUE;
 		for (int i = 0; i < arr.length; i++) {
